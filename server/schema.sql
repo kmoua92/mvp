e3af884f7a90d198c2091ea814ee58b8781e94d8
Ker Moua <kermoua92@gmail.com>
2016-10-18 12:23:51 -0700

    Add gameId to stats schema; create more test data

--- server/schema.sql
+++ server/schema.sql
@@ -26,7 +26,8 @@ CREATE TABLE stats (
   stl INTEGER,
   blk INTEGER,
   turnovers INTEGER,
-  pts INTEGER
+  pts INTEGER,
+  gameId INTEGER
 );
 
 -- ---
@@ -86,12 +87,79 @@ ALTER TABLE stats ADD FOREIGN KEY (opponentId) REFERENCES teams (teamId);
 -- Test Data
 -- ---
 
+-- TEST STATS
+
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('333','555','46','89','1','4','6','0','0','2','17');
+('001','002','46','89','2','3','6','1','0','2','21', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('002','002','39','82','3','4','4','1','1','1','13', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('003','002','57','94','3','4','3','0','0','0','15', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('004','002','44','88','0','9','6','1','1','1','25', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('005','002','69','78','0','8','5','0','2','2','19', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('001','002','53','90','1','4','4','0','0','1','18', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('002','002','44','85','0','4','6','1','0','1','10', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('003','002','41','88','3','3','2','0','1','0','17', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('004','002','66','89','0','9','6','0','2','1','25', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('005','002','53','80','0','9','5','0','2','0','11', '002');
+
+
+
+-- TEST PLAYERS
+
 INSERT INTO players VALUES
 -- id, name
-('333','John Doe');
+('001','Mike Bibby');
+
+INSERT INTO players VALUES
+-- id, name
+('002','Doug Christie');
+
+INSERT INTO players VALUES
+-- id, name
+('003','Peja Stojakovic');
+
+INSERT INTO players VALUES
+-- id, name
+('004','Chris Webber');
+
+INSERT INTO players VALUES
+-- id, name
+('005','Doug Christie');
+
+
+-- TEST TEAMS
+
 INSERT INTO teams VALUES
 -- id, name
-('555','Warriors');
+('001','Kings');
+
+INSERT INTO teams VALUES
+-- id, name
+('002','Lakers');
