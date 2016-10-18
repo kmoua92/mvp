b61e9fc7c64ee9836f4810b96f9c70018745c4e3
Ker Moua <kermoua92@gmail.com>
2016-10-17 18:45:32 -0700

    Set up server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+var http = require('http');
+
+var headers = {
+	'Content-Type': 'application/json'
+};
+
+
+var requestHandler = function(req, res) {
+	if (req.method === 'GET') {
+		res.writeHead(200, headers);
+		res.end('GET request received and handled');
+	}
+
+	if (req.method === 'POST') {
+		res.writeHead(200, headers);
+		res.end('POST request received and handled');
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = http.createServer(requestHandler);
+server.listen(3000);
+
+module.exports = server;
