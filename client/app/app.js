8d0c877d31fafca4f1ff09f4027c1a8bf608b4d9
Ker Moua <kermoua92@gmail.com>
2016-10-17 18:43:06 -0700

    Set up server

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,28 @@
 // Make request to server
+
+var app = {
+  server: 'http://127.0.0.1:3000'
+};
+
+app.init = function() {
+  app.fetch();
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    headers: {
+      'Content-Type': 'application/JSON'
+    },
+    success: function(data) {
+    	// data = JSON.parse(data);
+    	console.log(data);
+    }
+  });
+};
+
+
+$(function(){
+	app.init();
+});
