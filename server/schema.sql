ec99cf6927b5244bc2f24ea99c0e599517048196
Ker Moua <kermoua92@gmail.com>
2016-10-18 07:36:02 -0700

    Create sql database and schema tables w/test data

---
+++ server/schema.sql
@@ -0,0 +1,97 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP DATABASE if exists mvp;
+CREATE DATABASE mvp;
+USE mvp;
+
+DROP TABLE IF EXISTS stats;
+
+-- ---
+-- Table 'Stats'
+-- 
+-- ---
+
+		
+CREATE TABLE stats (
+  playerId INTEGER,
+  opponentId INTEGER,
+  fg INTEGER,
+  ft INTEGER,
+  3pm INTEGER,
+  reb INTEGER,
+  ast INTEGER,
+  stl INTEGER,
+  blk INTEGER,
+  turnovers INTEGER,
+  pts INTEGER
+);
+
+-- ---
+-- Table 'Players'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS players;
+		
+CREATE TABLE players (
+  id INTEGER PRIMARY KEY,
+  name INTEGER
+);
+
+-- ---
+-- Table 'Teams'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS teams;
+		
+CREATE TABLE teams (
+  id INTEGER PRIMARY KEY,
+  name INTEGER
+);
+
+
+-- ---
+-- Table 'Users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS users;
+    
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username VARCHAR(16),
+  password VARCHAR(16)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE stats ADD FOREIGN KEY (playerId) REFERENCES players (id);
+ALTER TABLE stats ADD FOREIGN KEY (opponentId) REFERENCES teams (id);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Stats` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Players` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Teams` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('333','555','46','89','1','4','6','0','0','2','17');
+INSERT INTO players VALUES
+-- id, name
+('333','John Doe');
+INSERT INTO teams VALUES
+-- id, name
+('555','Warriors');
