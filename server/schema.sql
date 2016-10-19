86857f7fb42118c4fe6ef17db3c4ead142e9874a
Ker Moua <kermoua92@gmail.com>
2016-10-18 17:22:07 -0700

    Add more test data to use for team queries

--- server/schema.sql
+++ server/schema.sql
@@ -18,6 +18,7 @@ DROP TABLE IF EXISTS stats;
 CREATE TABLE stats (
   playerId INTEGER,
   opponentId INTEGER,
+  homeId INTEGER,
   fg INTEGER,
   ft INTEGER,
   3pm INTEGER,
@@ -91,43 +92,83 @@ ALTER TABLE stats ADD FOREIGN KEY (opponentId) REFERENCES teams (teamId);
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('001','002','46','89','2','3','6','1','0','2','21', '001');
+('001','002', '001','46','89','2','3','6','1','0','2','21', '001');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('002','002','39','82','3','4','4','1','1','1','13', '001');
+('002','002', '001', '39','82','3','4','4','1','1','1','13', '001');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('003','002','57','94','3','4','3','0','0','0','15', '001');
+('003','002', '001', '57','94','3','4','3','0','0','0','15', '001');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('004','002','44','88','0','9','6','1','1','1','25', '001');
+('004','002', '001', '44','88','0','9','6','1','1','1','25', '001');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('005','002','69','78','0','8','5','0','2','2','19', '001');
+('005','002', '001', '69','78','0','8','5','0','2','2','19', '001');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('001','002','53','90','1','4','4','0','0','1','18', '002');
+('001','002', '001', '53','90','1','4','4','0','0','1','18', '002');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('002','002','44','85','0','4','6','1','0','1','10', '002');
+('002','002', '001', '44','85','0','4','6','1','0','1','10', '002');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('003','002','41','88','3','3','2','0','1','0','17', '002');
+('003','002', '001', '41','88','3','3','2','0','1','0','17', '002');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('004','002','66','89','0','9','6','0','2','1','25', '002');
+('004','002', '001', '66','89','0','9','6','0','2','1','25', '002');
 
 INSERT INTO stats VALUES
 -- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
-('005','002','53','80','0','9','5','0','2','0','11', '002');
+('005','002', '001', '53','80','0','9','5','0','2','0','11', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('006','001', '002', '46','89','2','3','6','1','0','2','18', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('007','001', '002', '39','85','3','4','4','1','1','1','29', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('008','001', '002', '57','94','3','4','3','0','0','0','15', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('009','001', '002', '44','88','0','9','6','1','1','1','12', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('010','001', '002', '77','65','0','12','5','0','2','2','31', '001');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('006','001', '002', '53','90','1','4','4','0','0','1','18', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('007','001', '002', '44','85','3','4','6','1','2','2','37', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('008','001', '002', '41','88','3','3','2','0','1','0','17', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('009','001', '002', '66','89','0','9','6','0','2','1','25', '002');
+
+INSERT INTO stats VALUES
+-- player, opponent, fg, ft, 3pm, reb, ast, stl, blk, turnovers, pts
+('010','001', '002', '63','69','0','12','5','0','3','1','32', '002');
 
 
 
@@ -152,6 +193,26 @@ INSERT INTO players VALUES
 INSERT INTO players VALUES
 -- id, name
 ('005','Vlade Divac');
+
+INSERT INTO players VALUES
+-- id, name
+('006','Derek Fisher');
+
+INSERT INTO players VALUES
+-- id, name
+('007','Kobe Bryant');
+
+INSERT INTO players VALUES
+-- id, name
+('008','Rick Fox');
+
+INSERT INTO players VALUES
+-- id, name
+('009','Robert Horry');
+
+INSERT INTO players VALUES
+-- id, name
+('010','Shaquille ONeal');
 
 
 -- TEST TEAMS
